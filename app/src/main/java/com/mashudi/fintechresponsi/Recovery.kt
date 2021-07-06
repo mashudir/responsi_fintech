8c6b2f882a85a2b118437f727e638a73601e6450
mashudi rohmat <mashudirohmat.mr@gmail.com>
2021-07-06 23:16:52 +0700

    UI Recovery

---
+++ app/src/main/java/com/mashudi/fintechresponsi/Recovery.kt
@@ -0,0 +1,11 @@
+package com.mashudi.fintechresponsi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Recovery : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recovery)
+    }
+}
